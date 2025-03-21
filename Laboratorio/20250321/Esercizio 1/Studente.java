468d03f9ccb45f3cfe81a826e34ae63b35c9d566
Eic17H <139933730+Eic17H@users.noreply.github.com>
2025-03-21 16:03:27 +0100

    Ereditarietà: primo esercizio

---
+++ Laboratorio/20250321/Esercizio 1/Studente.java
@@ -0,0 +1,48 @@
+public class Studente extends Persona{
+    private int matricola;
+    private int annoIscrizione;
+
+    public Studente(){
+        super();
+        this.matricola = 0;
+        this.annoIscrizione = 0;
+    }
+
+    public Studente(String nome, String cognome, int eta, int matricola, int annoIscrizione){
+        super(nome, cognome, eta);
+        this.matricola = matricola;
+        this.annoIscrizione = annoIscrizione;
+    }
+
+    public String getNome(){
+        return super.getNome();
+    }
+    public String getCognome(){
+        return super.getCognome();
+    }
+    public int getEta(){
+        return super.getEta();
+    }
+    public int getMatricola(){
+        return this.matricola;
+    }
+    public int getAnnoIscrizione(){
+        return this.annoIscrizione;
+    }
+
+    public void setNome(String nome){
+        super.setNome(nome);
+    }
+    public void setCogome(String cognome){
+        super.setCognome(cognome);
+    }
+    public void setEta(int eta){
+        super.setEta(eta);
+    }
+    public void setMatricola(int matricola){
+        this.matricola = matricola;
+    }
+    public void setAnnoIscrizione(int annoIscrizione){
+        this.annoIscrizione = annoIscrizione;
+    }
+}
